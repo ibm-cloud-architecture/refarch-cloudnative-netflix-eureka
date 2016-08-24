eb6fa567904332fbf4bad12f9e25324e5aa3818e
Rick Osowski <rosowski@gmail.com>
2016-08-24 14:55:56 -0500

    Initial deploy container group script, with sensible env overrides
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- bluemix/deploy-container-group.sh
+++ bluemix/deploy-container-group.sh
@@ -7,20 +7,25 @@ source $SCRIPTDIR/.bluemixrc
 # Validate image exists in Bluemix registry
 #################################################################################
 echo "Looking up Bluemix registry images"
-BLUEMIX_IMAGES=$(bluemix ic images --format "{{.Repository}}:{{.Tag}}")
+BLUEMIX_IMAGES=$(cf ic images --format "{{.Repository}}:{{.Tag}}")
 
 REQUIRED_IMAGES=(
-    ${EUREKA_IMAGE}
+    ${REGISTRY_IMAGE}
 )
 
 for image in ${REQUIRED_IMAGES[@]}; do
     echo "$BLUEMIX_IMAGES" | grep "$image" > /dev/null
     if [ $? -ne 0 ]; then
         #echo "Pulling ${DOCKERHUB_NAMESPACE}/$image from Dockerhub"
-        #bluemix ic cpi ${DOCKERHUB_NAMESPACE}/$image ${BLUEMIX_REGISTRY_HOST}/${BLUEMIX_REGISTRY_NAMESPACE}/$image
-        #TODO Fail and echo message that image must be built and pushed
-        #docker build -t registry.ng.bluemix.net/cloudarch/eureka .
-        #docker push registry.ng.bluemix.net/cloudarch/eureka
+        echo "Registry Image Not Found.  Attempting to push from Local Docker"
+        DOCKER_IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}")
+        echo "$DOCKER_IMAGES" | grep "$image" > /dev/null
+        if [ $? -ne 0 ]; then
+          echo "Local Registry Image Not Found.  Exiting..."
+          exit 1
+        fi
+        docker tag ${REGISTRY_IMAGE} ${BLUEMIX_REGISTRY_HOST}/${BLUEMIX_REGISTRY_NAMESPACE}/${REGISTRY_IMAGE}
+        docker push ${BLUEMIX_REGISTRY_HOST}/${BLUEMIX_REGISTRY_NAMESPACE}/${REGISTRY_IMAGE}
     fi
 done
 
@@ -28,9 +33,12 @@ done
 # Start Eureka container group
 #################################################################################
 echo "Starting Eureka container group"
-bluemix ic group-create --name eureka_cluster \
+cf ic group create --name eureka_cluster \
   --publish 8761 --memory 256 --auto \
   --min 1 --max 3 --desired 2 \
   --hostname $REGISTRY_HOSTNAME \
   --domain $ROUTES_DOMAIN \
-  ${BLUEMIX_REGISTRY_HOST}/${BLUEMIX_REGISTRY_NAMESPACE}/${EUREKA_IMAGE}
+  --env eureka.client.serviceUrl.defaultZone=${REGISTRY_URL} \
+  --env eureka.client.registerWithEureka=true \
+  --env eureka.client.fetchRegistry=true \
+  ${BLUEMIX_REGISTRY_HOST}/${BLUEMIX_REGISTRY_NAMESPACE}/${REGISTRY_IMAGE}
