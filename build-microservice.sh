8ec5bfc0420351b3d5a75659b6984493036ef413
edwin van de grift <edwinvandegrift@us.ibm.com>
2016-09-28 10:07:10 -0700

    Added new build-mocroservice script

--- build-microservice.sh
+++ build-microservice.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+IMAGE_NAME=microservices-refapp-eureka
+MAVEN_BUILD_TARGET=target/eureka-0.0.1-SNAPSHOT.jar
+GRADLE_BUILD_TARGET=build/libs/eureka-0.0.1-SNAPSHOT.jar
+
 while getopts "md" ARG; do
   case ${ARG} in
     m)
@@ -14,16 +18,16 @@ done
 if [[ ${USE_MAVEN} == 'yes' ]]; then
   mvn clean package
   if [[ ${DO_DOCKER} == 'yes' ]]; then
-    cp target/eureka-0.0.1-SNAPSHOT.jar docker/app.jar
+    cp ${MAVEN_BUILD_TARGET} docker/app.jar
   fi
 else
   ./gradlew clean build
   if [[ ${DO_DOCKER} == 'yes' ]]; then
-	cp build/libs/eureka-0.0.1-SNAPSHOT.jar docker/app.jar
+	cp ${GRADLE_BUILD_TARGET} docker/app.jar
   fi
 fi
 
 if [[ ${DO_DOCKER} == 'yes' ]]; then
   cd docker/
-  docker build -t microservices-refapp-eureka .
+  docker build -t ${IMAGE_NAME} .
 fi
