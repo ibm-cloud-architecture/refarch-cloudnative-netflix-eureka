f7fcbf458a2aab35e6fd5322d5713a26ead41575
Rick Osowski <rosowski@gmail.com>
2016-09-29 14:19:50 -0500

    Merged gradle and mvn build helpers to build-microservice.sh
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- build-gradle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./gradlew clean build
-
-cp build/libs/eureka-0.0.1-SNAPSHOT.jar docker/app.jar
-
-cd docker/
-
-docker build -t microservices-refapp-eureka .
--- build-mvn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn clean package
-
-cp target/eureka-0.0.1-SNAPSHOT.jar docker/app.jar
-
-cd docker/
-
-docker build -t microservices-refapp-eureka .
